2a8b8d7a3f7439219438abf033918764561b98c6
daniswastaken <daniswastaken@gmail.com>
2026-05-03 15:06:49 +0700

    feat: implement About page with custom Vue component and markdown content rendering

---
+++ src/components/parser/About.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+defineProps<{
+    mainTitle: string;
+    backgroundTitle: string;
+}>();
+</script>
+
+<template>
+    <div class="mb-10 mt-10">
+        <div
+            select-none
+            relative
+            h18
+            mt5
+            mb-2
+            pointer-events-none
+        >
+            <span
+                text-7em
+                color-transparent
+                absolute
+                left--6rem
+                top="1/2"
+                translate-y="-1/2"
+                font-black
+                leading-1em
+                text-stroke-1.5
+                text-stroke-hex-aaa
+                op20
+                dark:op20
+                uppercase
+                whitespace-nowrap
+                class="backgroundKey"
+            >
+                {{ backgroundTitle }}
+            </span>
+            <!-- The normal text on top -->
+            <span
+                absolute
+                text-3.5em
+                left--3rem
+                top="1/2"
+                translate-y="-1/2"
+                font-black
+                uppercase
+                whitespace-nowrap
+            >
+                {{ mainTitle }}
+            </span>
+        </div>
+    </div>
+</template>
---
+++ src/contents/about.md
@@ -0,0 +1,12 @@
+---
+mainTitle: Handaru
+backgroundTitle: Handaru Daniswara
+---
+
+Greetings! I'm Handaru Daniswara, a software engineer and researcher.
+
+My mindset all this time is, if you build out something, then even if just one singular person using it, then you already are a good programmer. With that mindset, I'm currently have more than 100K+ users accross my [projects](/projects).
+
+Outside of programming, I'm an active researcher—publishing several [papers](/paper) and occasionally making [blog post](/blog) about coding and stuff. 
+
+Let's walk the talk, hi@handaru.dev.
