cfaad520b9efd9568b7687caa427d356b3ec6de9
daniswastaken <daniswastaken@gmail.com>
2026-07-04 17:59:51 +0700

    feat: add miaq icons

---
+++ src/components/icons/Miaq.vue
@@ -0,0 +1,75 @@
+<template>
+    <svg
+        width="100"
+        height="100"
+        viewBox="0 0 100 100"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <rect
+            x="66.3359"
+            y="12"
+            width="7.12871"
+            height="27.3267"
+            fill="url(#paint0_linear_1035_11)"
+        />
+        <path
+            d="M20 12L29.505 23.2871L39.0099 12V39.3267H20V12Z"
+            fill="url(#paint1_linear_1035_11)"
+        />
+        <path
+            d="M27.1287 58.3369H32.4752L39.604 85.6636H20L27.1287 58.3369Z"
+            fill="url(#paint2_linear_1035_11)"
+        />
+        <path
+            d="M74.0605 58.3364C76.0291 58.3364 77.625 59.9323 77.625 61.9009V82.0991C77.6249 83.2176 77.1088 84.2143 76.3027 84.8677L80 91.604H73.4795L70.0635 85.6636H65.7441C63.7757 85.6636 62.1799 84.0675 62.1797 82.0991V61.9009C62.1797 59.9323 63.7756 58.3364 65.7441 58.3364H74.0605Z"
+            fill="url(#paint3_linear_1035_11)"
+        />
+        <defs>
+            <linearGradient
+                id="paint0_linear_1035_11"
+                x1="69.9003"
+                y1="12"
+                x2="69.9003"
+                y2="39.3267"
+                gradientUnits="userSpaceOnUse"
+            >
+                <stop stop-color="#D9D9D9" stop-opacity="0.85" />
+                <stop offset="1" stop-color="#737373" stop-opacity="0.5" />
+            </linearGradient>
+            <linearGradient
+                id="paint1_linear_1035_11"
+                x1="29.505"
+                y1="12"
+                x2="29.505"
+                y2="39.3267"
+                gradientUnits="userSpaceOnUse"
+            >
+                <stop stop-color="#D9D9D9" stop-opacity="0.85" />
+                <stop offset="1" stop-color="#737373" stop-opacity="0.5" />
+            </linearGradient>
+            <linearGradient
+                id="paint2_linear_1035_11"
+                x1="29.802"
+                y1="58.3369"
+                x2="29.802"
+                y2="85.6636"
+                gradientUnits="userSpaceOnUse"
+            >
+                <stop stop-color="#D9D9D9" stop-opacity="0.85" />
+                <stop offset="1" stop-color="#737373" stop-opacity="0.5" />
+            </linearGradient>
+            <linearGradient
+                id="paint3_linear_1035_11"
+                x1="69.9025"
+                y1="58.3364"
+                x2="69.9025"
+                y2="85.6632"
+                gradientUnits="userSpaceOnUse"
+            >
+                <stop stop-color="#D9D9D9" stop-opacity="0.85" />
+                <stop offset="1" stop-color="#737373" stop-opacity="0.5" />
+            </linearGradient>
+        </defs>
+    </svg>
+</template>
